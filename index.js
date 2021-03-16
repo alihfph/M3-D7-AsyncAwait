f1f9cfbe9c7092e3f89a4bbcf6d06f543579b7fe
bioback <78535998+bioback@users.noreply.github.com>
2021-03-16 15:32:34 +0100

    ex1 and 2

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 console.log("Hey")
 
-container.innerHTML = ""
+// container.innerHTML = ""
 
+let users = []
 
-let cardTemlate = (userInfo)=>{
+let cardTemplate = (user1)=>{
   return `<div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">${userInfo.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <h5 class="card-title">${user1.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${user1.email}</h6>
+    <p class="card-text">${user1.address.street}</p>
   </div>
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Info
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">${user1.name}</a>
+    <a class="dropdown-item" href="#">${user1.username}</a>
+    <a class="dropdown-item" href="#">${user1.email}</a>
+  </div>
+</div>
 </div>`
+
 }
+
+// let dropdownTemplate = (useroptions)=>{
+//   return `<div class="dropdown">
+//   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//     Dropdown button
+//   </button>
+//   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+//     <a class="dropdown-item" href="#">${useroptions.name}</a>
+//     <a class="dropdown-item" href="#">${useroptions.username}</a>
+//     <a class="dropdown-item" href="#">${useroptions.email}</a>
+//   </div>
+// </div>`
+// }
+
+// const createDropdown = () => {
+//   const dropdown = document.getElementById("dropdown");
+//   // container.innerHTML = ""
+//     dropdown.innerHTML += dropdownTemplate(users)
+//   };
+console.log(cardTemplate)
 const createCards = () => {
   const container =document.getElementById("container");
   container.innerHTML = "";
-  secondimages.forEach((image) => {
-    container.innerHTML += returnCardSecond(image.url);
+  users.forEach((userInfo) => {
+    container.innerHTML += cardTemplate(userInfo);
   });
 };
 
@@ -26,10 +56,11 @@ window.onload =()=>{
   fetch(
     "https://jsonplaceholder.typicode.com/users"
   )
-    .then((response) => {
+    
+  .then((response) => {
       return response.json();
     })
     .then((parsedBody) => {
-      console.log(parsedBody);
+      users = parsedBody
   }).catch(err=>(err))
 }
